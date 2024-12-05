c95b9e22941387f37246cdc32b9b6876224d9c22
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2024-12-05 19:59:03 +0100

    Refactor App component structure and add Header, Footer, and Hero components

---
+++ src/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <h2>Footer</h2>
+        </footer>
+    );
+}
---
+++ src/component/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+
+    return (
+        <header>
+            <nav>
+                <ul className='nav_top'>
+                    <li>Human Ocean</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
