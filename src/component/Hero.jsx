492049ff8738019b0f134b2bbf2773178c998ee3
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2024-12-05 19:59:05 +0100

    Add Hero component and remove txt file

---
+++ src/component/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <section className='hero'>
+            <h1>Découvrez les parallèles fascinants entre notre corps et l’océan</h1>
+        </section>
+    );
+}
+
