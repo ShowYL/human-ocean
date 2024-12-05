ebd290e5a032a216cba85207624c3e73f2ca410e
Yann L <130548077+ShowYL@users.noreply.github.com>
2024-12-05 20:57:12 +0100

    Remove unused navigation items from Footer component

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,12 +4,6 @@ export default function Footer() {
 
     return (
         <footer>
-                <ul className='nav_bottom'>
-                    <li>Human Ocean</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            
                 <p className='credit'>© 2021 Human Ocean</p>
         </footer>
     );
