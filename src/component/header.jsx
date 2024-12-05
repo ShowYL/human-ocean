8c91393f970513ff41e7056877047348ff12fad2
IAidenI <bolerichardnathan@gmail.com>
2024-12-05 23:20:22 +0100

    Ajout background animné

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import logo from '../ressources/logo.png'
+import logo from '../ressources/logo.png';
 
 export default function Header() {
-
     return (
-        <header>
-            <nav>
-                <ul className='nav_top'>
-                    <li><img src={logo} className="logo"/>Human Ocean</li>
-                    <li>Crédits</li>
-                    <li>Captcha</li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <div className="bubbles">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <header>
+                <nav>
+                    <ul className="nav_top">
+                        <li>
+                            <img src={logo} alt="Logo" className="logo" />
+                            Human Ocean
+                        </li>
+                        <li>Crédits</li>
+                        <li>Captcha</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 }
