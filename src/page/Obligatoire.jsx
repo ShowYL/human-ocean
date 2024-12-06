35d51e7fb6678005e7ca4537413427c000a66eb4
IAidenI <bolerichardnathan@gmail.com>
2024-12-06 03:44:26 +0100

    Corps humain --> Carrousel

--- src/page/Obligatoire.jsx
+++ src/page/Obligatoire.jsx
@@ -10,11 +10,11 @@ export default function Obligatoire() {
             <hr />
             <div className='panel'>
                 <div className='left-panel'>
-                    <h2>Les Fonction du corps Humain</h2>
+                    <h2>Les fonctions du corps Humain</h2>
                     <CorpsHumain />
                 </div>
                 <div className='right-panel'>
-                <h2>Les Fonction de l'ocean</h2>
+                <h2>Les fonctions de l'ocean</h2>
                 </div>
             </div>
         </section>
